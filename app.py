b5cb4e215fec9d65c0782ceb901cee261b5538c6
tony <tony@gmail.com>
2025-06-09 03:49:25 +0800

    完成線上更新資料庫邏輯

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 from datetime import datetime
 import pandas as pd
 import pymysql
-from uvi import get_uvi_data_from_mysql
+from uvi import get_uvi_data_from_mysql, update_db
+import json
 
 
 app = Flask(__name__)
@@ -12,6 +13,16 @@ app = Flask(__name__)
 def index():
     columns, datas = get_uvi_data_from_mysql()
     return render_template("index.html", columns=columns, datas=datas)
+
+
+@app.route("/update_db")
+def update_uvi_db():
+    count, message = update_db()
+    nowtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    result = json.dumps(
+        {"時間": nowtime, "更新筆數": count, "結果": message}, ensure_ascii=False
+    )
+    return result
 
 
 if __name__ == "__main__":
--- uvi.py
+++ uvi.py
@@ -2,6 +2,42 @@ import pandas as pd
 import pymysql
 
 
+# 更新資料庫
+def update_db():
+    api_url = "https://data.moenv.gov.tw/api/v2/uv_s_01?api_key=9b651a1b-0732-418e-b4e9-e784417cadef&limit=1000&sort=datacreationdate%20desc&format=CSV"
+    sqlstr = """
+        insert ignore into uvi(sitename,uvi,unit,county,wgs84_lon,wgs84_lat,datacreationdate)
+        values(%s,%s,%s,%s,%s,%s,%s)
+    """
+    row_count = 0
+    message = ""
+    try:
+        # 讀取最新的雲端資料
+        df = pd.read_csv(api_url)
+        df["datacreationdate"] = pd.to_datetime(df["datacreationdate"], format="mixed")
+        df.dropna()
+        df1 = df[df["uvi"] >= 0]
+        values = df1.values.tolist()
+        # 寫入資料庫
+        conn = open_db()
+        cur = conn.cursor()
+        cur.executemany(sqlstr, values)
+        row_count = cur.rowcount
+        conn.commit()
+
+        print(f"更新{row_count}筆資料成功!!!")
+        message = "更新資料庫成功!!!"
+
+    except Exception as e:
+        print(e)
+        message = f"更新資料失敗:{e}"
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return row_count, message
+
+
 # 開啟資料庫
 def open_db():
     conn = None
@@ -41,7 +77,4 @@ def get_uvi_data_from_mysql():
 
 # 本地運行
 if __name__ == "__main__":
-    conn = open_db
-    print(conn)
-    columns, datas = get_uvi_data_from_mysql()
-    print(columns)
+    update_db()
