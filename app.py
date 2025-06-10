98373e6a3c7c2da384eb096c1a22f6102e0e8e1a
tony <tony@gmail.com>
2025-06-10 18:57:01 +0800

    第一次嘗試繪圖 失敗

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
 import pymysql
@@ -9,12 +9,66 @@ import json
 app = Flask(__name__)
 
 
+#
+@app.route("/filter", methods=["POST"])
+def filter_data():
+    # args=> form
+    county = request.form.get("county")
+    columns, datas = get_uvi_data_from_mysql()
+    df = pd.DataFrame(datas, columns=columns)
+    # 取得特定縣市的資料
+    df1 = (
+        df.groupby("county")
+        .get_group(county)
+        .groupby("sitename")["uvi"]
+        .mean()
+        .round(2)
+    )
+    print(df1)
+    return {"county": county}
+
+
 @app.route("/")
 def index():
+    # 取得資料庫最新的資料
     columns, datas = get_uvi_data_from_mysql()
-    return render_template("index.html", columns=columns, datas=datas)
+    # 取出不同縣市給select
+    df = pd.DataFrame(datas, columns=columns)
+    # 排序縣市
+    counties = sorted(df["county"].unique().tolist())
+
+    # 選取縣市後的資料(預設ALL)
+    county = request.args.get("county", "ALL")
+
+    df = pd.DataFrame(datas, columns=columns)
+
+    if county != "ALL":
+        # 取得特定縣市的資料
+        df1 = df.groupby("county").get_group(county)
+        columns = df1.columns.tolist()
+        datas = df1.values.tolist()
+
+    uvi_by_county = (
+        df.groupby("county")["uvi"]
+        .mean()
+        .round(2)
+        .reset_index()
+        .rename(columns={"county": "name", "uvi": "value"})
+    )
+
+    uvi_data = uvi_by_county.to_dict(orient="records")
+
+    return render_template(
+        "index.html",
+        columns=columns,
+        datas=datas,
+        counties=counties,
+        selected_county=county,
+        uvi_data=uvi_data,
+    )
 
 
+# 更新資料庫
 @app.route("/update_db")
 def update_uvi_db():
     count, message = update_db()
